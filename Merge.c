dd2f040c02fe5c5c750842a4e1849a1e304e57ac
hyroxl <tylerpaul926@icloud.com>
2023-10-22 13:02:00 -0400

    added main c file and readme

---
+++ Merge.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void swap(int *a, int *b);
+void randomize(int arr[], int n);
+void merge(int arr[], int l, int m, int r);
+void mergeSort(int arr[],int l, int r);
+int main(int argc, char **argv)
+{
+    int size = atoi(argv[1]) + 1;
+    float startTimeRand = (float)clock() / CLOCKS_PER_SEC;
+    int randArr[size];
+    for (int i = 1; i < size; i++)
+    {
+        randArr[i] = i;
+    }
+    randomize(randArr, size);
+    float endTimeRand = (float)clock() / CLOCKS_PER_SEC;
+    FILE *fptr;
+    fptr = fopen("randArr.txt", "w");
+    for (int j = 0; j < size; j++){
+        fprintf(fptr, "%d\n", randArr[j]);
+    }
+    fclose(fptr);
+    float startTimeMerge = (float)clock() / CLOCKS_PER_SEC;
+    mergeSort(randArr, 0, size - 1);
+    float endTimeMerge = (float)clock() / CLOCKS_PER_SEC;
+    float timeElapedRand = endTimeRand - startTimeRand;
+    float timeElapsedMerge = endTimeMerge - startTimeMerge;
+    fptr = fopen("sortedArr.txt", "w");
+    for (int k = 0; k < size; k++){
+       fprintf(fptr, "%d\n", randArr[k]);
+    }
+    printf("Randomization Time:%f\nSort Time:%f\n", timeElapedRand, timeElapsedMerge);
+}
+
+// A utility function to swap to integers
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+// A function to generate a random permutation of arr[]
+void randomize ( int arr[], int n )
+{
+    // Use a different seed value so that we don't get same
+    // result each time we run this program
+    srand ( time(NULL) );
+ 
+    // Start from the last element and swap one by one. We don't
+    // need to run for the first element that's why i > 0
+    for (int i = n -1; i > 0; i--)
+    {
+        // Pick a random index from 0 to i
+        int j = rand() % (i+1);
+ 
+        // Swap arr[i] with the element at random index
+        swap(&arr[i], &arr[j]);
+    }
+}
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 = r - m; 
+  
+    // Create temp arrays 
+    int L[n1], R[n2]; 
+  
+    // Copy data to temp arrays 
+    // L[] and R[] 
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1 + j]; 
+  
+    // Merge the temp arrays back 
+    // into arr[l..r] 
+    // Initial index of first subarray 
+    i = 0; 
+  
+    // Initial index of second subarray 
+    j = 0; 
+  
+    // Initial index of merged subarray 
+    k = l; 
+    while (i < n1 && j < n2) { 
+        if (L[i] <= R[j]) { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    // Copy the remaining elements 
+    // of L[], if there are any 
+    while (i < n1) { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    // Copy the remaining elements of 
+    // R[], if there are any 
+    while (j < n2) { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+// l is for left index and r is 
+// right index of the sub-array 
+// of arr to be sorted 
+void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) { 
+        // Same as (l+r)/2, but avoids 
+        // overflow for large l and h 
+        int m = l + (r - l) / 2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m + 1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
---
+++ readme.md
@@ -0,0 +1,4 @@
+#c mergesort algorythm
+
+
+a lot of it is ripped from geeksforgeeks.com and stack overflow but it works :sob:
